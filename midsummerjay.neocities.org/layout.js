9cc8f3a7b4bce0ddf7a5e81c0fe5372197faaf15
jay <avioletqueen@gmail.com>
2024-09-17 10:40:14 -0500

    first commit

---
+++ midsummerjay.neocities.org/layout.js
@@ -0,0 +1,80 @@
+const nesting = getNesting();
+
+document.addEventListener("DOMContentLoaded", function () {
+  loadLayoutByPetraPixel();
+});
+
+function loadLayoutByPetraPixel() {
+  const mainEl = document.querySelector("main");
+  if (!mainEl) return;
+  mainEl.insertAdjacentHTML("beforebegin", headerHTML());
+  mainEl.insertAdjacentHTML("afterend", footerHTML());
+  giveActiveClassToLinks();
+}
+
+function headerHTML() {
+  // ${nesting} outputs "./" or "../" depending on current page depth.
+  // You can use it to refer to images etc.
+  // Example: <img src="${nesting}img/logo.png"> might output <img src="../img/logo.png">
+
+  return `
+<header>
+
+        <div class="header-content">
+			<div class="header-title">jay's spot (under construction)</div>
+	        <nav>
+	          <ul>
+	            <li><a href="https://midsummerjay.neocities.org">home</a></li>
+	          </ul>
+	        </>
+		</div>
+      </header>
+	  
+            <aside class="left-sidebar">
+	  
+        <div class="sidebar-section">
+          <div class="sidebar-title">bio</div>
+          <p>hello i am jay and here is my neocities. i am a gamer girl i use she/her he/him and i love dorothea arnault. i do not remember how to use html and i am never open to feedback.</p>
+        </div>
+      </aside>
+	
+      `;
+}
+
+function footerHTML() {
+  // ${nesting} outputs "./" or "../" depending on current page depth.
+  // You can use it to refer to images etc.
+  // Example: <img src="${nesting}img/logo.png"> might output <img src="../img/logo.png">
+
+  return `
+<footer><div>Footer Text. <a href="#">Link.</a> Template generated with <a href="https://petrapixel.neocities.org/coding/layout-generator.html">petrapixel's layout generator</a>.</div></footer>`;
+}
+
+function getNesting() {
+  const numberOfSlashes = window.location.pathname.split("/").length - 1;
+  if (numberOfSlashes == 1) return "./";
+  return "../".repeat(numberOfSlashes - 1);
+}
+
+function giveActiveClassToLinks() {
+  const els = document.querySelectorAll("nav a");
+  [...els].forEach((el) => {
+    const href = el.getAttribute("href").replace(".html", "").replace("#", "");
+    const pathname = window.location.pathname.replace("/public/", "");
+
+    if (href == "/" || href == "/index.html") {
+      if (window.location.href == "http://localhost:8080/" || pathname == "/") {
+        el.classList.add("active");
+      }
+    } else {
+      if (window.location.href.includes(href)) {
+        el.classList.add("active");
+
+        if (el.closest("summary")) {
+          el.closest("details").addAttribute("open");
+          el.closest("summary").classList.add("active");
+        }
+      }
+    }
+  });
+}
