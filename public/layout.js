7d0d36748a9f9962af77600bd893b252059c8995
jay <avioletqueen@gmail.com>
2024-09-17 11:06:20 -0500

    make chatbox much shorter

--- public/layout.js
+++ public/layout.js
@@ -38,7 +38,7 @@ function headerHTML() {
         </div>
         <div class="sidebar-section">
           <div class="sidebar-title">chatbox</div>
-          <iframe src="https://www3.cbox.ws/box/?boxid=3540886&boxtag=85Jf6C" width="100%" height="450" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>
+          <iframe src="https://www3.cbox.ws/box/?boxid=3540886&boxtag=85Jf6C" width="100%" height="225" allowtransparency="yes" allow="autoplay" frameborder="0" marginheight="0" marginwidth="0" scrolling="auto"></iframe>
         </div>
       </aside>
 	
