500fd17c550d127c0548f598089d4702d8bd07bd
jay <avioletqueen@gmail.com>
2024-09-25 10:26:33 -0500

    updated visitor counter

--- public/layout.js
+++ public/layout.js
@@ -45,7 +45,7 @@ function headerHTML() {
         </nav>
         <div class="sidebar-section">
         <div class="sidebar-title">visitors</div>
-        <iframe srcdoc="<div style='text-align:center;padding-top:8px;'><script language='javascript' type='text/javascript' src='//counter1.fc2.com/views.php?id=39945556&main=1'></script><noscript><img src='//counter1.fc2.com/counter_now.php?id=39945556&main=1' /></noscript></div>" width="100%" frameborder="0" marginheight="0" marginwidth="0" height="30px"></iframe>
+        <iframe srcdoc="<div style='text-align:center;padding-top:8px;'><script language="javascript" type="text/javascript" src="//counter1.fc2.com/counter.php?id=39945556&main=1"></script><noscript><img src="//counter1.fc2.com/counter_img.php?id=39945556&main=1" /></noscript></div>" width="100%" frameborder="0" marginheight="0" marginwidth="0" height="30px"></iframe>
         </div>
         <div class="sidebar-section">
           <div class="sidebar-title">chatbox</div>
