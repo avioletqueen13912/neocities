49699a3c493583ac090644c9bc6e86bdafc1e661
jay <avioletqueen@gmail.com>
2024-10-25 09:04:45 -0500

    status cafe styling

--- public/layout.js
+++ public/layout.js
@@ -73,8 +73,7 @@ function headerHTML() {
             <p style="text-align:center;margin-top:-5px;"><a href="https://hillhouse.neocities.org/journal/notes/palestine"><img src="${nesting}/images/palestinegif.gif" height="25px"></a></p>
 	  <div class="sidebar-section">
           <div class="sidebar-title">latest update</div>
-          <iframe srcdoc="<div id='statuscafe'><div id='statuscafe-username'></div><div id='statuscafe-content'></div></div><script src='https://status.cafe/current-status.js?name=midsummerjay' defer></script>" width="100%" frameborder="0" marginheight="0" marginwidth="0"></iframe>
-          <p>10/18/2024 - my about page is finally completed!! or at least sensibly filled out, i guess.</p>
+          <iframe srcdoc="<link href='./statuscafe.css' rel='stylesheet' /><div id='statuscafe'><div id='statuscafe-content'></div><div id='statuscafe-username'></div></div><script src='https://status.cafe/current-status.js?name=midsummerjay' defer></script>" width="100%" frameborder="0" marginheight="0" marginwidth="0" height="100px"></iframe>
         </div>
         
             <div class="sidebar-section">
