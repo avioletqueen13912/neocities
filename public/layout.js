31943470d5a97c7e9495b765716b1223b73a1904
jay <avioletqueen@gmail.com>
2024-10-25 13:16:29 -0500

    status dynamic sizing attempt

--- public/layout.js
+++ public/layout.js
@@ -74,7 +74,17 @@ function headerHTML() {
             <p style="text-align:center;margin-top:-5px;"><a href="https://hillhouse.neocities.org/journal/notes/palestine"><img src="${nesting}/images/palestinegif.gif" height="25px"></a></p>
 	  <div class="sidebar-section">
           <div class="sidebar-title">latest update</div>
-          <iframe srcdoc="<link href='${nesting}/statuscafe.css' rel='stylesheet' /><div id='statuscafe'><div id='statuscafe-content'></div><div id='statuscafe-username'></div></div><script src='https://status.cafe/current-status.js?name=midsummerjay' defer></script>" width="100%" frameborder="0" marginheight="0" marginwidth="0" height="125px"></iframe>
+          <iframe 
+            srcdoc="<link href='${nesting}/statuscafe.css' rel='stylesheet' /><div id='statuscafe'><div id='statuscafe-content'></div><div id='statuscafe-username'></div></div><script src='https://status.cafe/current-status.js?name=midsummerjay' defer></script>" 
+            width="100%" frameborder="0" marginheight="0" marginwidth="0" id="update">
+          </iframe>
+          <script>
+            let div = document.getElementById("update");
+            div.onload = function () {
+            div.style.height =
+                div.contentWindow.document.body.scrollHeight + 'px';
+            }
+          </script>
         </div>
         
             <div class="sidebar-section">
@@ -104,8 +114,9 @@ function headerHTML() {
         <div class="sidebar-section">
           <div class="sidebar-title">to-do</div>
           <ul>
-          <li>add my own button!!</li>
           <li>add my tamanotchi</li>
+          <li>fix menu bolding on active links</li>
+          <li>add my own button!!</li>
           <li>collect more blinkies + stamps</li>
           <li>tweak formatting on "latest blog posts" page</li>
           <li>find dividers + graphics i like</li>
